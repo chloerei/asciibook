10ddd047438b1b69a98680e627835e5be90a4942
Rei <chloerei@gmail.com>
2020-01-14 17:33:18 +0800

    extract base builder

---
+++ lib/asciibook/builders/base_builder.rb
@@ -0,0 +1,56 @@
+module Asciibook
+  module Builders
+    class BaseBuilder
+      def initialize(book)
+        @book = book
+        @base_dir = @book.options[:base_dir]
+        @exclude_patterns = ["build/**/*"]
+      end
+
+      def build
+        FileUtils.mkdir_p @build_dir
+        FileUtils.rm_r Dir.glob("#{@build_dir}/*")
+
+        generate_pages
+        copy_assets
+      end
+
+      def generate_pages
+        layout = Liquid::Template.parse(File.read(File.join(@theme_dir, 'layout.html')))
+        @book.pages.each do |page|
+          File.open(File.join(@build_dir, page.path), 'w') do |file|
+            file.write layout.render({
+              'book' => @book.to_hash,
+              'page' => page.to_hash
+            })
+          end
+        end
+      end
+
+      def copy_assets
+        Dir.glob('**/*.{jpg,png,gif,mp3,mp4,ogg,wav}', File::FNM_CASEFOLD, base: @base_dir).each do |path|
+          if !exclude_file?(path)
+            copy_file(path, @base_dir, @build_dir)
+          end
+        end
+
+        Dir.glob('**/*.{jpb,png,gif,svg,css,js}', File::FNM_CASEFOLD, base: @theme_dir).each do |path|
+          copy_file(path, @theme_dir, @build_dir)
+        end
+      end
+
+      def exclude_file?(path)
+        @exclude_patterns.any? do |pattern|
+          File.fnmatch?(pattern, path)
+        end
+      end
+
+      def copy_file(path, src_dir, dest_dir)
+        src_path = File.join(src_dir, path)
+        dest_path = File.join(dest_dir, path)
+        FileUtils.mkdir_p File.dirname(dest_path)
+        FileUtils.cp src_path, dest_path
+      end
+    end
+  end
+end
--- lib/asciibook/builders/pdf_builder.rb
+++ lib/asciibook/builders/pdf_builder.rb
@@ -1,14 +1,11 @@
 module Asciibook
   module Builders
-    class PdfBuilder
+    class PdfBuilder < BaseBuilder
       def initialize(book)
-        @book = book
-        @base_dir = @book.options[:base_dir]
+        super
         @build_dir = File.expand_path('build/pdf', @base_dir)
         @theme_dir = File.expand_path('../../../../themes/default/pdf', __FILE__)
         @theme_config = YAML.safe_load(File.read(File.join(@theme_dir, 'config.yml')))
-
-        @exclude_patterns = ["build/**/*"]
       end
 
       def build
@@ -19,43 +16,6 @@ module Asciibook
         copy_assets
         generate_header_footer
         generate_pdf
-      end
-
-      def generate_pages
-        layout = Liquid::Template.parse(File.read(File.join(@theme_dir, 'layout.html')))
-        @book.pages.each do |page|
-          File.open(File.join(@build_dir, page.path), 'w') do |file|
-            file.write layout.render({
-              'book' => @book.to_hash,
-              'page' => page.to_hash
-            })
-          end
-        end
-      end
-
-      def copy_assets
-        Dir.glob('**/*.{jpg,png,gif,mp3,mp4,ogg,wav}', File::FNM_CASEFOLD, base: @base_dir).each do |path|
-          if !exclude_file?(path)
-            copy_file(path, @base_dir, @build_dir)
-          end
-        end
-
-        Dir.glob('**/*.{jpb,png,gif,svg,css,js}', File::FNM_CASEFOLD, base: @theme_dir).each do |path|
-          copy_file(path, @theme_dir, @build_dir)
-        end
-      end
-
-      def exclude_file?(path)
-        @exclude_patterns.any? do |pattern|
-          File.fnmatch?(pattern, path)
-        end
-      end
-
-      def copy_file(path, src_dir, dest_dir)
-        src_path = File.join(src_dir, path)
-        dest_path = File.join(dest_dir, path)
-        FileUtils.mkdir_p File.dirname(dest_path)
-        FileUtils.cp src_path, dest_path
       end
 
       def generate_header_footer
